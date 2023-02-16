c433f03cc0c95f680d040302cbc157373876eaf0
[Prathiba] <sanprathi@gmail.com>
2023-02-16 15:07:01 +1100

    first commit

---
+++ src/demo_git/demo_01.java
@@ -0,0 +1,9 @@
+package demo_git;
+
+public class demo_01 {
+	public static void main(String[] args)
+	{
+		System.out.println("Hello");
+	}
+
+}
---
+++ src/demo_git/test.java
@@ -0,0 +1,9 @@
+package demo_git;
+
+public class test {
+	public static void main (String[] args)
+	{
+		System.out.println("This is to chec k the tracking");
+	}
+
+}
